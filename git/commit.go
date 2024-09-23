f566c45cb8b3ed6a8c4075fccd3918234d843dc9
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-09-23 20:23:24 -0300

    Fix structure of commit logic

--- git/commit.go
+++ git/commit.go
@@ -3,20 +3,41 @@ package git
 import (
 	"fmt"
 	"go.uber.org/zap"
+	"os"
 	"os/exec"
 )
 
-func CreateCommit(repoPath string, message string, logger *zap.Logger) error {
-	cmd := exec.Command("git", "-C", repoPath, "add", ".")
+type Repository struct {
+	URL    string
+	Name   string
+	Logger *zap.Logger
+}
+
+func (r *Repository) Clone() error {
+	if _, err := os.Stat(r.Name); os.IsNotExist(err) {
+		r.Logger.Info("Cloning repository...", zap.String("RepoURL", r.URL))
+		cmd := exec.Command("git", "clone", r.URL)
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to clone repo: %w", err)
+		}
+		r.Logger.Info("Repository cloned successfully")
+	} else {
+		r.Logger.Info("Repository already exists locally")
+	}
+	return nil
+}
+
+func (r *Repository) CreateCommit(message string) error {
+	cmd := exec.Command("git", "-C", r.Name, "add", ".")
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to add changes: %w", err)
 	}
 
-	cmd = exec.Command("git", "-C", repoPath, "commit", "-m", message)
+	cmd = exec.Command("git", "-C", r.Name, "commit", "-m", message)
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to create commit: %w", err)
 	}
 
-	logger.Info("Commit created", zap.String("Message", message))
+	r.Logger.Info("Commit created", zap.String("Message", message))
 	return nil
 }
