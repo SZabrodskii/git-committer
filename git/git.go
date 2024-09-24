6633ca66d92a8543afaee2d6d2a9c54976cf0bc1
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-09-23 21:26:10 -0300

    Fix struct of git

--- git/git.go
+++ git/git.go
@@ -2,53 +2,36 @@ package git
 
 import (
 	"github.com/SZabrodskii/git-committer/config"
-
-	"fmt"
 	"go.uber.org/zap"
-	"math/rand"
 	"path/filepath"
-	"time"
 )
 
-func GenerateRandomCommitsPerDay(min int, max int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max-min+1) + min
-}
-
-func Run(logger *zap.Logger, config *config.Config) {
+func Run(logger *zap.Logger, cfg *config.Config) {
 	logger.Info("Git Auto Committer CLI initialized")
 
-	err := CloneRepo(config.RepoURL, logger)
-	if err != nil {
+	repo := &Repository{
+		URL:    cfg.RepoURL,
+		Name:   filepath.Base(cfg.RepoURL),
+		Logger: logger,
+	}
+
+	if err := repo.Clone(); err != nil {
 		logger.Fatal("Failed to clone repo", zap.Error(err))
 	}
 
-	repoName := filepath.Base(config.RepoURL)
+	gitCommitter := &GitCommitter{
+		MinCommits:        cfg.MinCommits,
+		MaxCommits:        cfg.MaxCommits,
+		Days:              cfg.Days,
+		IncludeWeekends:   cfg.IncludeWeekends,
+		WeekendMinCommits: cfg.WeekendMinCommits,
+		WeekendMaxCommits: cfg.WeekendMaxCommits,
+		RepoURL:           cfg.RepoURL,
+		CommitTemplate:    cfg.CommitTemplate,
+		Repo:              repo,
+	}
 
-	for i := 0; i < config.Days; i++ {
-		currentDay := time.Now().AddDate(0, 0, i)
-		weekday := currentDay.Weekday()
-
-		if !config.IncludeWeekends && (weekday == time.Saturday || weekday == time.Sunday) {
-			logger.Info("Skipping weekend", zap.String("Day", currentDay.String()))
-			continue
-		}
-
-		var commitsToday int
-		if weekday == time.Saturday || weekday == time.Sunday {
-			commitsToday = GenerateRandomCommitsPerDay(config.WeekendMinCommits, config.WeekendMaxCommits)
-		} else {
-			commitsToday = GenerateRandomCommitsPerDay(config.MinCommits, config.MaxCommits)
-		}
-
-		for j := 0; j < commitsToday; j++ {
-			message := fmt.Sprintf("%s %d", config.CommitTemplate, j+1)
-			err = CreateCommit(repoName, message, logger)
-			if err != nil {
-				logger.Fatal("Failed to create commit", zap.Error(err))
-			}
-		}
-
-		logger.Info("Commits created for day", zap.Time("Day", currentDay), zap.Int("Commits", commitsToday))
+	if err := gitCommitter.generateCommits(); err != nil {
+		logger.Fatal("Failed to generate commits", zap.Error(err))
 	}
 }
