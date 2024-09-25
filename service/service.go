913a835643ca26623d5b196f5c8e44c54e68e1be
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-09-25 18:55:50 -0300

    Feat service for commit

--- service/service.go
+++ service/service.go
@@ -1,44 +1,38 @@
 package service
 
 import (
-	"fmt"
-	"golang.org/x/net/html"
+	"errors"
+	"io/ioutil"
 	"net/http"
-	"strings"
+	"regexp"
 )
 
-func GetRandomAnekdot() (string, error) {
+type AnekdotService struct{}
+
+func NewAnekdotService() *AnekdotService {
+	return &AnekdotService{}
+}
+
+func (as *AnekdotService) GetRandomAnekdot() (string, error) {
 	resp, err := http.Get("https://www.anekdot.ru/random/anekdot/")
 	if err != nil {
-		return "", fmt.Errorf("failed to fetch joke: %w", err)
+		return "", errors.New("failed to fetch anekdot")
 	}
 	defer resp.Body.Close()
 
-	doc := html.NewTokenizer(resp.Body)
-	var joke string
-	found := false
-	for {
-		tt := doc.Next()
-		switch tt {
-		case html.ErrorToken:
-			if joke == "" {
-				return "", fmt.Errorf("failed to parse joke")
-			}
-			return joke, nil
-		case html.StartTagToken, html.SelfClosingTagToken:
-			t := doc.Token()
-			if t.Data == "div" {
-				for _, attr := range t.Attr {
-					if attr.Key == "class" && attr.Val == "text" {
-						found = true
-					}
-				}
-			}
-		case html.TextToken:
-			if found {
-				joke = strings.TrimSpace(doc.Token().Data)
-				found = false
-			}
-		}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.New("failed to read response body")
 	}
+
+	re := regexp.MustCompile(`<div class="text">(.+?)</div>`)
+	matches := re.FindStringSubmatch(string(body))
+
+	if len(matches) < 2 {
+		return "", errors.New("anekdot not found")
+	}
+
+	anekdot := matches[1]
+	anekdot = regexp.MustCompile(`<[^>]*>`).ReplaceAllString(anekdot, "")
+	return anekdot, nil
 }
