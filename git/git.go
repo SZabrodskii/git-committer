ccc6d94e84ced5122cecc9823db1524e64747e6b
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-09-25 18:54:44 -0300

    Add work with service

--- git/git.go
+++ git/git.go
@@ -2,11 +2,12 @@ package git
 
 import (
 	"github.com/SZabrodskii/git-committer/config"
+	"github.com/SZabrodskii/git-committer/service"
 	"go.uber.org/zap"
 	"path/filepath"
 )
 
-func Run(logger *zap.Logger, cfg *config.Config) {
+func Run(logger *zap.Logger, cfg *config.Config, anekdotService *service.AnekdotService) {
 	logger.Info("Git Auto Committer CLI initialized")
 
 	repoPath := filepath.Base(cfg.RepoURL)
@@ -17,13 +18,15 @@ func Run(logger *zap.Logger, cfg *config.Config) {
 		Logger: logger,
 	}
 
-	if err := repo.Clone(); err != nil {
-		logger.Fatal("Failed to clone repo", zap.Error(err))
+	if err := repo.Init(); err != nil {
+		logger.Fatal("Failed to initialize repo", zap.Error(err))
 	}
 
 	gitCommitter := NewGitCommitter(cfg, repo)
 
-	gitCommitter.Commit(logger)
+	if err := gitCommitter.generateCommits(anekdotService); err != nil {
+		logger.Fatal("Failed to generate commits", zap.Error(err))
+	}
 
 	logger.Info("Commits generated successfully!")
 }
