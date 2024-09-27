6676a93c8c5632bbe870cb55183e2b21bfa96dd2
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-09-27 14:39:09 -0300

    Feat Define Git-related operations and repository structure for better management

--- git/git.go
+++ git/git.go
@@ -1,30 +1,17 @@
 package git
 
 import (
-	"github.com/SZabrodskii/git-committer/config"
-	"github.com/SZabrodskii/git-committer/service"
 	"go.uber.org/zap"
-	"path/filepath"
 )
 
-func Run(logger *zap.Logger, cfg *config.Config, anekdotService *service.AnekdotService) {
+func Run(logger *zap.Logger, gitCommitter *GitCommitter) {
 	logger.Info("Git Auto Committer CLI initialized")
 
-	repoPath := filepath.Base(cfg.RepoURL)
-
-	repo := &Repository{
-		URL:    cfg.RepoURL,
-		Name:   repoPath,
-		Logger: logger,
-	}
-
-	if err := repo.Init(); err != nil {
+	if err := gitCommitter.Repo.Init(); err != nil {
 		logger.Fatal("Failed to initialize repo", zap.Error(err))
 	}
 
-	gitCommitter := NewGitCommitter(cfg, repo)
-
-	if err := gitCommitter.generateCommits(anekdotService); err != nil {
+	if err := gitCommitter.generateCommits(); err != nil {
 		logger.Fatal("Failed to generate commits", zap.Error(err))
 	}
 
