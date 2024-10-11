003bdc4efdaa4e96c39ec8c5c976ba541bdc6fc4
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-10-11 17:14:49 -0300

    Add: tests for repository.go

--- git/repository_test.go
+++ git/repository_test.go
@@ -1,29 +1,66 @@
 package git
 
 import (
-	"github.com/stretchr/testify/assert"
+	"github.com/SZabrodskii/git-committer/config"
+	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
+	"os"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
-func NewTestLogger() (*zap.Logger, error) {
-	config := zap.NewProductionConfig()
-	config.OutputPaths = []string{"stdout"}
-	config.ErrorOutputPaths = []string{"stdout"}
-	return config.Build()
+type RepositoryTestSuite struct {
+	suite.Suite
+	repo *Repository
 }
 
-func TestCreateCommit_ErrorAdding(t *testing.T) {
-	logger, err := NewTestLogger()
-	if err != nil {
-		t.Fatalf("failed to create logger: %v", err)
-	}
+func (suite *RepositoryTestSuite) SetupSuite() {
+	logger, err := zap.NewProduction()
+	suite.Require().NoError(err)
 
-	repo := &Repository{
-		Name:   "/invalid/path",
-		Logger: logger,
-	}
+	suite.repo = NewRepository(&config.Config{RepoURL: "", RepoName: "."}, logger) // Используем текущую директорию
+}
 
-	err = repo.CreateCommit("Test commit")
-	assert.Error(t, err, "expected an error, got nil")
+func (suite *RepositoryTestSuite) TestInitExistingRepo() {
+	err := suite.repo.Init()
+	suite.Require().NoError(err)
+	suite.repo.Logger.Info("Repository directory already exists locally")
+}
+
+func (suite *RepositoryTestSuite) TestCreateCommit() {
+	err := suite.repo.Init()
+	suite.Require().NoError(err, "Failed to init repository")
+
+	fileName := "test_file.txt"
+
+	f, err := os.Create(fileName)
+	suite.Require().NoError(err)
+	defer f.Close()
+
+	defer func() {
+		err = os.Remove(fileName)
+		suite.Require().NoError(err, "Failed to delete file: %s", fileName)
+	}()
+
+	_, err = os.Stat(fileName)
+	suite.Require().NoError(err, "File should exist at: %s", fileName)
+
+	commitMessage := "Test commit message"
+	err = suite.repo.CreateCommit(fileName, commitMessage)
+	suite.Require().NoError(err)
+
+	suite.repo.Logger.Info("Commit created", zap.String("Message", commitMessage))
+
+	cmd := exec.Command("git", "-C", suite.repo.Name, "log", "--pretty=format:%s", "-n", "1")
+	output, err := cmd.Output()
+	suite.Require().NoError(err)
+
+	history := strings.Split(string(output), "\n")
+	suite.Equal(1, len(history))
+	suite.Contains(history[0], commitMessage)
+}
+
+func TestRepositoryTestSuite(t *testing.T) {
+	suite.Run(t, new(RepositoryTestSuite))
 }
